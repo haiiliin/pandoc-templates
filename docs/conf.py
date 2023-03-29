065fbff1e6a843174e83a611ec610dfd75224e51
Hailin-Wang <hailin.wang@connect.polyu.hk>
2023-03-29 20:06:27 +0800

    Add LaTeX options

--- docs/conf.py
+++ docs/conf.py
@@ -32,3 +32,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md', 'defaults']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Options for LaTeX output ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
+
+latex_show_urls = 'footnote'
+latex_show_pagerefs = True
